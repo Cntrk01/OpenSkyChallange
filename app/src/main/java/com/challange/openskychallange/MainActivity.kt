d4d5e4c8648b721894affff040d3a32942cd991c
Mustafa Cantürk Karabulut <karabulut_3828@hotmail.com>
2026-01-16 18:35:16 +0300

    -Google Maps added and running. -Version incompatibility issues resolved. -Key added to Properties.

--- app/src/main/java/com/challange/openskychallange/MainActivity.kt
+++ app/src/main/java/com/challange/openskychallange/MainActivity.kt
@@ -4,15 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.challange.openskychallange.ui.theme.OpenSkyChallangeTheme
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             OpenSkyChallangeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    GoogleMapScreen()
                 }
             }
         }
@@ -31,17 +40,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun GoogleMapScreen() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OpenSkyChallangeTheme {
-        Greeting("Android")
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(
+            LatLng(41.015137, 28.979530), // İstanbul
+            10f
+        )
+    }
+
+    var isMapLoad by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState,
+            onMapLoaded = {
+                isMapLoad = true
+            }
+        ) {
+            Marker(
+                state = MarkerState(
+                    position = LatLng(41.015137, 28.979530)
+                ),
+                title = "İstanbul",
+                snippet = "Google Maps Compose"
+            )
+        }
+        if (!isMapLoad) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
     }
 }
